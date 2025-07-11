5997711a979c4e77eb624a45edae460f2112ddc9
Endertul0 <shawn.shireman.5@gmail.com>
2025-07-10 22:14:21 -0700

    initial commit

---
+++ src/main/java/net/endertul/abilmod/AbilitiesMod.java
@@ -0,0 +1,16 @@
+package net.endertul.abilmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class AbilitiesMod implements ModInitializer {
+	public static final String MOD_ID = "abilities-mod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/endertul/abilmod/AbilitiesModClient.java
@@ -0,0 +1,10 @@
+package net.endertul.abilmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AbilitiesModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
